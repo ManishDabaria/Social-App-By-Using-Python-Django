66bc64614683fdcb1185bd794d4a149b7fe51d66
Manish <manish786vermario@gmail.com>
2022-07-07 23:17:22 +0530

    this third attempt to update new features

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('settings', views.settings, name="settings"),
+    path('upload', views.upload, name="upload"),
+    path('like-post', views.like_post, name="like-post"),
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('logout', views.logout, name="logout"),
--- core/views.py
+++ core/views.py
@@ -1,25 +1,48 @@
 import re
-from .models import Profile
 import django
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import Profile, Post
 
 
 
 @login_required(login_url='signin')
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
+
+
 
 @login_required(login_url='signin')
+def upload(request):
+    
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
 
+        
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
+
+
+@login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
-    if request.method == "POST":
 
+    if request.method == 'POST':
+        
         if request.FILES.get('image') == None:
             image = user_profile.profileimg
             bio = request.POST['bio']
@@ -29,7 +52,6 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-
         if request.FILES.get('image') != None:
             image = request.FILES.get('image')
             bio = request.POST['bio']
@@ -39,10 +61,14 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-
+        
         return redirect('settings')
-    
     return render(request, 'setting.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    pass
 
 
 def signup(request):
@@ -101,6 +127,7 @@ def signin(request):
 
     else:
         return render(request, 'signin.html')
+
 
 @login_required(login_url='signin')
 def logout(request):
