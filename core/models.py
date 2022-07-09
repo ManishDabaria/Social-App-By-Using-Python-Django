e7eb48ba102138aadcba0043a5e1265d4e8a3d6e
Manish <manish786vermario@gmail.com>
2022-07-09 23:39:59 +0530

    this fifth time update

--- core/models.py
+++ core/models.py
@@ -42,7 +42,7 @@ class LikePost(models.Model):
     def __str__ (self):
         return self.username
 
-class FollowerCount(models.Model):
+class FollowersCount(models.Model):
     follower = models.CharField(max_length=100)
     user = models.CharField(max_length=100)
 
--- core/views.py
+++ core/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post, LikePost
-
+from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 
 @login_required(login_url='signin')
@@ -14,6 +14,20 @@ from .models import Profile, Post, LikePost
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
+
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_lists = list(chain(*feed))
 
     posts = Post.objects.all()
     return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
@@ -40,7 +54,17 @@ def upload(request):
 @login_required(login_url='signin')
 def follow(request):
     if request.method == 'POST':
-        pass
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+ user)
     else:
         return redirect('/')
 
@@ -104,12 +128,25 @@ def profile(request, pk):
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
 
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
 
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts' : user_posts,
         'user_post_length' : user_post_length,
+        'button_text': button_text,
+        'user_followers': user_followers,
+        'user_following': user_following,
     }
     return render(request, 'profile.html', context)
 
