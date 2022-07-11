ce27a2c21c7d23282b878779f6d62c850e0ed3ad
Manish <manish786vermario@gmail.com>
2022-07-11 22:56:03 +0530

    Post feed and Search Update

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from locale import currency
 import re
+from typing import final
 import django
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -7,7 +9,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
 from itertools import chain
-
+import random
 
 @login_required(login_url='signin')
 # Create your views here.
@@ -29,9 +31,32 @@ def index(request):
 
     feed_lists = list(chain(*feed))
 
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts':posts})
+    # user suggestion starts
+    all_users = User.objects.all()
+    user_following_all = []
 
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestion_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestion_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append()
+    
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(users.id)
+    
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+    
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts':feed_lists, 'suggestions_username_profile_list':suggestions_username_profile_list[:4]})
 
 
 @login_required(login_url='signin')
@@ -67,6 +92,33 @@ def follow(request):
             return redirect('/profile/'+ user)
     else:
         return redirect('/')
+
+
+@login_required(login_url='signin')
+def search(request):
+
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+        
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+
+        username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list':username_profile_list})    
+
+
 
 
 @login_required(login_url='signin')
